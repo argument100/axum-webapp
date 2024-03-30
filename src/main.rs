059ba124338ee89f63037b8c14d63ca347feccb6
argument100 <100blendy@gmail.com>
2024-03-30 23:20:19 +0900

    In the middle of Chapter 6

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 #[tokio::main]
 async fn main() {
-    let app = axum::Router::new().route("/", axum::routing::get(|| async {"Hello, World!"}));
+    let app = axum::Router::new()
+        .route("/", axum::routing::get(handler_top))
+        .route("/other", axum::routing::get(handler_other));
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler_top() -> String {
+    "Hello, World!".to_string()
+}
+
+async fn handler_other() -> String {
+    "This is other page...".to_string()
 }
