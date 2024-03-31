3c175f7f17be3b051bdeeb9ec03677ca6fc4acf1
argument100 <100blendy@gmail.com>
2024-03-31 21:59:07 +0900

    In the middle of Chapter 6

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 async fn main() {
     let app = axum::Router::new()
         .route("/", axum::routing::get(handler_top))
-        .route("/other", axum::routing::get(handler_other));
+        .route("/usr/:id/:user", axum::routing::get(handler_param))
+        .route("/qry", axum::routing::get(handler_query));
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -14,6 +15,11 @@ async fn handler_top() -> String {
     "Hello, World!".to_string()
 }
 
-async fn handler_other() -> String {
-    "This is other page...".to_string()
+async fn handler_param(axum::extract::Path((id, user)): axum::extract::Path<(usize, String)>) -> String {
+    format!("User ID: {}. name: {}.", id, user)
+}
+
+async fn handler_query(axum::extract::Query(params):
+                       axum::extract::Query<std::collections::HashMap<String, String>>) -> String {
+    format!("id: {}, name: {}", params["id"], params["name"])
 }
